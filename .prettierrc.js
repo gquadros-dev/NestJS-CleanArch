37a006558ed5405569e7dc575ca19e7506c9c799
Gabriel Quadros <gabrielfernand464508@gmail.com>
2024-02-14 17:28:38 -0300

    raw: setting dev environment

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {impoprt("prettier").Config} */
+
+const config = {
+  semi: false,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 80,
+  arrowParens: "avoid"
+}
+
+module.exports = config;
